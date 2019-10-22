4a5e037787500f57953ced632e5e33a590a4eb59
dotoryyy <honey8025@gmail.com>
2019-10-22 17:04:20 +0900

    vue commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <span v-on:click="addTodo" class="addContainer">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
+
+        <Modal v-if="showModal" @close="showModal = false">
+        <h3 slot="header">
+            경고!
+            <i class="fas fa-times closeModalBtn" @click="showModal = false"></i>
+        </h3>
+        <div slot="body">아무것도 입력하지 않으셨습니다.</div>
+        </Modal>
+    </div>
+</template>
+
+<script>
+import Modal from './common/Modal.vue'
+export default {
+    data: function() {
+        return{
+            newTodoItem: "",
+            showModal: false
+        }
+    },
+    methods: {
+        addTodo: function() {
+            if (this.newTodoItem !== '') {
+                this.$emit('addTodoItem', this.newTodoItem)
+                this.clearInput();
+            } else {
+                this.showModal = !this.showModal;
+            }
+        },
+        clearInput: function() {
+            this.newTodoItem = '';
+        }
+    },
+    components: {
+        Modal : Modal
+    }
+}
+</script>
+
+<style scoped>
+    input:focus {
+     outline: none;   
+    }
+    .inputBox{
+        background: #fff;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input {
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer {
+        float: right;
+        background: linear-gradient(to right, #6478FB, #9763FB);
+        display: block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn {
+        color: #fff;
+        vertical-align: middle;
+    }
+</style>
